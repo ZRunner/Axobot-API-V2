3c0b69eaae65806fb2cd6a3ddcc3e6c8fd420358
ZRunner <z.runner.mc@gmail.com>
2023-06-22 11:31:13 -0400

    feat(core): add express app

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello world!");
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("Hello world!");
+});
+
+app.listen(3000, () => {
+    console.info(`App V${process.env.npm_package_version} is running on http://localhost:3000 !`);
+});
