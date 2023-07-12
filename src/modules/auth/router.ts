826c00ccc55b023fadfbed0dae5140d9d1d8fca8
ZRunner <z.runner.mc@gmail.com>
2023-07-11 21:36:39 -0400

    feat(discord): make callback POST

--- src/modules/auth/router.ts
+++ src/modules/auth/router.ts
@@ -1,12 +1,12 @@
 import express from "express";
 
-import { getDiscordCallback, getMe } from "./controler";
+import { getDiscordCallback as postDiscordCallback, getMe } from "./controler";
 import { tokenCheckMiddleware } from "./tokens";
 
 const router = express.Router();
 
 router.get("/me", tokenCheckMiddleware, getMe);
 
-router.get("/discord-callback", getDiscordCallback);
+router.post("/discord-callback", postDiscordCallback);
 
 export default router;
