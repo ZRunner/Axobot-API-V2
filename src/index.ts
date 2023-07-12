4f7b76e4e9d278f25f0e417edeb9935c26923303
ZRunner <z.runner.mc@gmail.com>
2023-07-11 22:49:46 -0400

    refact(cors): simplify origin check

--- src/index.ts
+++ src/index.ts
@@ -33,26 +33,8 @@ ConsoleStamp(console, {
 });
 
 // Set up CORS
-const allowedOrigins = [
-    "http://axobeta.zrunner.me",
-    "http://axobot.zrunner.me",
-];
 app.use(cors({
-    origin: function(origin, callback) { // allow requests with no origin
-        // (like mobile apps or curl requests)
-        if (!origin) {
-           return callback(null, true);
-        }
-        // allow localhost origin
-        if (/^http:\/\/localhost(:[0-9]+)?$/.test(origin)) {
-            return callback(null, true);
-        }
-        if (allowedOrigins.indexOf(origin) === -1) {
-          const msg = "The CORS policy for this site does not allow access from the specified Origin.";
-          return callback(new Error(msg), false);
-        }
-        return callback(null, true);
-      },
+    origin: "http://axobeta.zrunner.me",
 }));
 
 // Allow BigInt in JSON responses
