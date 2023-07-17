a9ebbc6fd54ee9f2e1d6b176108754f6b85b52a2
ZRunner <z.runner.mc@gmail.com>
2023-07-16 23:24:31 -0400

    feat(discord): add more methods

--- src/bot/client.ts
+++ src/bot/client.ts
@@ -1,8 +1,12 @@
 import { Client, Events, GatewayIntentBits, PermissionResolvable } from "discord.js";
 
 import Database from "../database/db";
+import GuildConfigData from "../database/guild-config";
+import { AllRepresentation } from "../database/guild-config-types";
 import { DBRawUserData } from "../database/models/users";
 import { isDiscordAPIError } from "../modules/discord/types/typeguards";
+
+type ConfigValueType = AllRepresentation["default"];
 
 export default class DiscordClient {
     private static instance: DiscordClient;
@@ -10,6 +14,8 @@ export default class DiscordClient {
     private client: Client | null = null;
 
     private db: Database = Database.getInstance();
+
+    private defaultGuildConfig: GuildConfigData = GuildConfigData.getInstance();
 
     private constructor() {}
 
@@ -82,7 +88,7 @@ export default class DiscordClient {
         try {
             return await this.getClient().guilds.fetch(guildId);
         } catch (err) {
-            if (isDiscordAPIError(err) && err.code === 10013) {
+            if (isDiscordAPIError(err) && err.code === 10004) {
                 return null;
             }
             throw err;
@@ -117,6 +123,46 @@ export default class DiscordClient {
             return false;
         }
         return member.permissions.has(permission);
+    }
+
+    public async getGuildMemberIds(guildId: string) {
+        const guild = await this.resolveGuild(guildId);
+        if (guild === null) {
+            return null;
+        }
+        console.debug(guild.shard === undefined);
+        const guildMembers = await guild.members.fetch();
+        console.debug(guildMembers);
+        return guild.members.fetch().then(members => members.map(member => BigInt(member.id)));
+    }
+
+    public async getDefaultGuildConfig() {
+        return await this.defaultGuildConfig.getOptionsList();
+    }
+
+    public async getGuildConfig(guildId: bigint) {
+        const setupOptions = await this.db.getGuildConfig(guildId);
+        console.debug(setupOptions);
+        const defaultConfig = await this.defaultGuildConfig.getOptionsList();
+        const config: {[key: string]: ConfigValueType} = {};
+        for (const [optionName, value] of Object.entries(defaultConfig)) {
+            const option = setupOptions.find((item) => item.option_name === optionName);
+            if (option === undefined) {
+                config[optionName] = value.default;
+            } else {
+                config[optionName] = await this.defaultGuildConfig.convertToType(option.option_name, option.value);
+            }
+        }
+        return config;
+    }
+
+    public async getGuildConfigValue(guildId: bigint, optionName: string) {
+        const dbValue = await this.db.getGuildConfigValue(guildId, optionName);
+        if (dbValue !== null) {
+            return await this.defaultGuildConfig.convertToType(optionName, dbValue);
+        }
+        const defaultConfig = await this.defaultGuildConfig.getOptionsList();
+        return defaultConfig[optionName].default;
     }
 
 }
