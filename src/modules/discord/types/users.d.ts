75490adca9a80e3a9f95b57d70a706fe4104d024
ZRunner <z.runner.mc@gmail.com>
2024-03-31 21:58:25 -0400

    refact(discord): fetch user data from DiscordClient class

--- src/modules/discord/types/users.d.ts
+++ src/modules/discord/types/users.d.ts
@@ -4,3 +4,12 @@ interface AuthenticatedUserObject {
     globalName: string | null;
     avatar: string;
 }
+
+interface OauthUserData {
+    id: string;
+    avatar: string | null;
+    global_name: string | null;
+    locale: string;
+    public_flags: number;
+    username: string;
+}
